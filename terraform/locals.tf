07ca07cc85c97ecfd34499cbaad909a363b58a86
Evgeny Arbatov <arbatovevgeny@gmail.com>
2026-02-21 18:32:32 +0700

    Remove unused variables

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,10 +1,14 @@
 locals {
-  openweather_api_key = coalesce(
-    var.openweather_api_key,
-    trimspace(element(split("=", file("/Users/zhenya/gitRepo/[private]/openweather.env")), length(split("=", file("/Users/zhenya/gitRepo/[private]/openweather.env"))) - 1))
+  openweather_api_key = trimspace(
+    element(
+      split("=", file("/Users/zhenya/gitRepo/[private]/openweather.env")),
+      length(split("=", file("/Users/zhenya/gitRepo/[private]/openweather.env"))) - 1
+    )
   )
-  tomtom_api_key = coalesce(
-    var.tomtom_api_key,
-    trimspace(element(split("=", file("/Users/zhenya/gitRepo/[private]/tomtom.env")), length(split("=", file("/Users/zhenya/gitRepo/[private]/tomtom.env"))) - 1))
+  tomtom_api_key = trimspace(
+    element(
+      split("=", file("/Users/zhenya/gitRepo/[private]/tomtom.env")),
+      length(split("=", file("/Users/zhenya/gitRepo/[private]/tomtom.env"))) - 1
+    )
   )
 }
